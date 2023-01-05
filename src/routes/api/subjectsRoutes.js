317a107f4a96a55a3139b1d29970331f2a908dfc
tiago-rodrigues1 <tiagords127@gmail.com>
2023-01-05 14:59:00 -0300

    muda "materias" para "subjects"

--- src/routes/api/subjectsRoutes.js
+++ src/routes/api/subjectsRoutes.js
@@ -7,23 +7,23 @@ const dbMiddleware = require('../../middlewares/db');
 
 router.use(dbMiddleware);
 
-router.get("/materias/", async (req, res) => {
+router.get("/subjects/", async (req, res) => {
     await new SubjectController().listar(req, res);
 });
 
-router.post("/materias/buscar", async (req, res) => {
+router.post("/subjects/buscar", async (req, res) => {
     await new SubjectController().buscar(req, res);
 });
 
-router.post("/materias/", async (req, res) => {
+router.post("/subjects/", async (req, res) => {
     await new SubjectController().adicionar(req, res); 
 });
 
-router.delete("/materias/", async (req, res) => {
+router.delete("/subjects/", async (req, res) => {
     await new SubjectController().deletar(req, res); 
 });
 
-router.put("/materias/", async (req, res) => {
+router.put("/subjects/", async (req, res) => {
     await new SubjectController().atualizar(req, res); 
 });
 
