66dae786ee58ff46a393fc64839ebd8725719af2
tiago-rodrigues1 <tiagords127@gmail.com>
2023-01-05 14:58:17 -0300

    subjects no cache

--- src/middlewares/cache.js
+++ src/middlewares/cache.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const router = express.Router();
 const nodeCache = require('node-cache');
 const axios = require('axios').default;
+
+const router = express.Router();
 
 const secondsIn8Hours = 8 * 60 * 60;
 const cache = new nodeCache({ stdTTL: secondsIn8Hours });
@@ -9,14 +10,26 @@ const cache = new nodeCache({ stdTTL: secondsIn8Hours });
 router.use(async (req, res, next) => {
     try {
         if (cache.has("activities")) {
-            req.cache = cache.get("activities");
+            req.activities = cache.get("activities");
         } else {
             const url = "http://localhost:8080/api/activities";
             const response = await axios.get(url);
 
             if (response.status === 200) {
-                cache.set("activities", response.data);
-                req.cache = cache.get("activities");
+                cache.set("activities", response.data.activities);
+                req.activities = cache.get("activities");
+            }
+        }
+
+        if (cache.has("subjects")) {
+            req.subjects = cache.get("subjects");
+        } else {
+            const url = "http://localhost:8080/api/subjects";
+            const response = await axios.get(url);
+
+            if (response.status === 200) {
+                cache.set("subjects", response.data.subjects);
+                req.subjects = cache.get("subjects");
             }
         }
 
